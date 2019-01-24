8fff50387846e9fe2a93dc23fb565c428c3d9717
mmarth <michael.marth@bragi.com>
2019-01-24 19:07:26 +0100

    initil commit with project struture for mobile + tv

---
+++ mobile/src/main/java/com/a7tv/codingchallenge/codingchallengefor7tv/AppEntryActivity.kt
@@ -0,0 +1,12 @@
+package com.a7tv.codingchallenge.codingchallengefor7tv
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AppEntryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_entry)
+    }
+}
