6f11bf1fabad5af9f101a140c6f811d143d22378
mm_dev_playground <michael.marth@bragi.com>
2019-01-25 21:28:18 +0100

    migrated to androidx

--- mobile/src/main/java/com/a7tv/codingchallenge/codingchallengefor7tv/AppEntryActivity.kt
+++ mobile/src/main/java/com/a7tv/codingchallenge/codingchallengefor7tv/AppEntryActivity.kt
@@ -1,7 +1,7 @@
 package com.a7tv.codingchallenge.codingchallengefor7tv
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class AppEntryActivity : AppCompatActivity() {
 
